565714d74d4061d5b88d220e3b04e6e5af1b8fb2
Matthew <mattghanno@gmail.com>
2020-07-04 17:10:47 +1000

    Finished 1st version of episodes page

--- js/episodes.js
+++ js/episodes.js
@@ -1,34 +1,38 @@
+var platformSelected = "yt";
+
 function initPositionHr() {
     $(".selected").css({
         display: "inline-block",
         width: $(".youtube").width(),
         position: "relative",
         left: (($(".youtube").position().left - 10) + "px"),
-        transitionTimingFunction: "ease-in-out"
+        transition: "0.75s left ease-in-out"
     });
 }
 
 initPositionHr();
 
+function fbPositionHr() {
+    $(".selected").css({
+        width: $(".facebook").width(),
+        left: (($(".facebook").position().left - 10) + "px")
+    });
+}
+
 function platformHover() {
-    $(".youtube").hover(function() {
-        initPositionHr();
+    $(".youtube").hover(initPositionHr, function() {
+        if (platformSelected == "fb") fbPositionHr();
     });
 
-    $(".instagram").hover(function() {
-        $(".selected").css({
-            width: $(".instagram").width(),
-            left: (($(".instagram").position().left - 10) + "px"),
-            transitionTimingFunction: "ease-in-out"
-        });
-    });
+    // $(".instagram").hover(function() {
+    //     $(".selected").css({
+    //         width: $(".instagram").width(),
+    //         left: (($(".instagram").position().left - 10) + "px")
+    //     });
+    // });
 
-    $(".facebook").hover(function() {
-        $(".selected").css({
-            width: $(".facebook").width(),
-            left: (($(".facebook").position().left - 10) + "px"),
-            transitionTimingFunction: "ease-in-out"
-        });
+    $(".facebook").hover(fbPositionHr, function() {
+        if (platformSelected == "yt") initPositionHr();
     });
 }
 
@@ -39,11 +43,15 @@ $(window).resize(function() {
 });
 
 $(".facebook").click(function() {
-    $(".fb-video").css({display: "block"});
+    $(".fb").css({display: "block"});
     $(".yt").css({display: "none"});
+
+    platformSelected = "fb";
 });
 
 $(".youtube").click(function() {
-    $(".fb-video").css({display: "none"});
+    $(".fb").css({display: "none"});
     $(".yt").css({display: "block"});
+
+    platformSelected = "yt";
 });
